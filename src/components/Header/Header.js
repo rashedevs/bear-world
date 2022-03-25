91eafac8b8e20f337c8fdcdb23b2baba81de1056
rashedevs <rasheduap2015@gmail.com>
2022-03-25 21:57:14 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Bear World</h1>
+            <h4>Your beloved teddy</h4>
+        </div>
+    );
+};
+
+export default Header;
